d1e1c6b5c87e7055913ffa73e7bc62b0777c2970
uros <uros.vardiic@gmail.com>
2019-03-07 20:26:48 +0100

    Updated FileSystemManager to support runtime dependencies

--- src/main/java/manager/FileSystemManager.java
+++ src/main/java/manager/FileSystemManager.java
@@ -12,49 +12,54 @@ import static util.Preconditions.checkNotNull;
 
 public class FileSystemManager {
 
-    private static final List<FileSystemInfo> registeredSystems = new ArrayList<>();
+    private static final List<FileSystem> registeredSystems = new ArrayList<>();
 
     private FileSystemManager() {}
 
     @NotNull
     @CallerSensitive
     public static FileSystem getFileSystem(@NotNull final Class<? extends FileSystem> caller) {
-        initializeFileSystemClass(caller);
-
-        // Finds the first file system that matches the callers class and returns it.
-        // If no such file system exists method throws an IllegalArgumentException.
-        return registeredSystems.stream()
-                .filter(fileSystemInfo -> fileSystemInfo.getFileSystem().getClass().equals(caller))
-                .findFirst().map(FileSystemInfo::getFileSystem)
-                .orElseThrow(() -> new IllegalArgumentException("Suitable file system wasn't found!"));
-
+        return getFileSystemWorker(checkNotNull(caller).getName());
     }
 
-    private static void initializeFileSystemClass(final Class<? extends FileSystem> caller) {
+    @NotNull
+    @CallerSensitive
+    public static FileSystem getFileSystem(@NotNull final String callerClassName) {
+        return getFileSystemWorker(checkNotNull(callerClassName));
+    }
+
+    private static FileSystem getFileSystemWorker(final String callerClassName) {
+        final Class<?> callerClass = initializeCallerClass(callerClassName);
+
+        // Finds the registered system that matches the specified caller,
+        // by comparing their classes,
+        // else if no systems were found throws an exception.
+        return registeredSystems.stream()
+                .filter(fileSystem -> fileSystem.getClass().equals(callerClass))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Suitable file system wasn't found!"));
+    }
+
+    private static Class<?> initializeCallerClass(final String callerClassName) {
         try {
-            Class.forName(caller.getName());
+            return Class.forName(callerClassName);
         } catch (final ClassNotFoundException ex) {
-            ex.printStackTrace();
+            throw new IllegalArgumentException("Specified caller class could not be found!");
         }
     }
 
     public static void registerSystem(@NotNull final FileSystem fileSystem) {
-        checkNotNull(fileSystem);
+        checkNotNull(fileSystem, "Null value can't be registered or unregistered!");
+        checkArgument(!registeredSystems.contains(fileSystem), "Specified system is already registered!");
 
-        final FileSystemInfo systemInfo = new FileSystemInfo(fileSystem);
-
-        checkArgument(!registeredSystems.contains(systemInfo), "Specified system is already registered!");
-
-        registeredSystems.add(systemInfo);
+        registeredSystems.add(fileSystem);
     }
 
     public static void unregisterSystem(@NotNull final FileSystem fileSystem) {
-        checkNotNull(fileSystem);
+        checkNotNull(fileSystem, "Null value can't be registered or unregistered!");
+        checkArgument(registeredSystems.contains(fileSystem), "Specified system could't be found!");
 
-        final FileSystemInfo systemInfo = new FileSystemInfo(fileSystem);
-
-        checkArgument(registeredSystems.contains(systemInfo), "Specified system couldn't be found!");
-
-        registeredSystems.remove(systemInfo);
+        registeredSystems.remove(fileSystem);
     }
+
 }
